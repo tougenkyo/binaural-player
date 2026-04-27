8b9416262ba95b9d19f31cfcca37e9d4c8123268
tougenkyo <74737083+tougenkyo@users.noreply.github.com>
2026-04-27 22:34:52 +0900

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,10 +1,9 @@
 @echo off
-chcp 65001 > nul
 cd /d "%~dp0"
 python binaural_player.py
 if %errorlevel% neq 0 (
     echo.
-    echo エラーが発生しました。Python がインストールされているか確認してください。
+    echo Error: Python is required.
     echo https://www.python.org/downloads/
     pause
 )
